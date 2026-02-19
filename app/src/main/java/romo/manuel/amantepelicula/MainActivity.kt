b53048dbd2e5d3847fe1423e3e4ba7529e956081
manuel-romo <manuel.romo253080@potros.itson.edu.mx>
2026-02-19 14:54:56 -0700

    Práctica2

--- app/src/main/java/romo/manuel/amantepelicula/MainActivity.kt
+++ app/src/main/java/romo/manuel/amantepelicula/MainActivity.kt
@@ -1,6 +1,10 @@
 package romo.manuel.amantepelicula
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +20,24 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val tvUserName = findViewById<EditText>(R.id.etUserName)
+        val tvPassword = findViewById<EditText>(R.id.etPassword)
+        val btnLogin = findViewById<TextView>(R.id.btnLogIn)
+
+        btnLogin.setOnClickListener {
+
+            val userName : String = tvUserName.text.toString().trim()
+            val password : String = tvPassword.text.toString().trim()
+
+            if(userName == "" || password == ""){
+                Toast.makeText(this, "Usuario y/o contraseña inválidos", Toast.LENGTH_SHORT).show();
+            } else{
+                val intent = Intent(this, ActivityMovie::class.java)
+                startActivity(intent)
+            }
+
+        }
+
     }
 }
